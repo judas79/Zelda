8d90776f0bfcbffe5c0aac85027e0a7eb92aa1ce
Rodman Underhill <robotrods@gmail.com>
2021-05-10 23:01:42 -0400

    update tutorial 28 published

--- Zelda/Assets/Scripts/SoundManager.cs
+++ Zelda/Assets/Scripts/SoundManager.cs
@@ -4,15 +4,51 @@ using UnityEngine;
 
 public class SoundManager : MonoBehaviour
 {
+    // T28 Singleton
+    public static SoundManager Instance = null;
+
+    // T28 Sound clips for link
+    //  public AudioClip 'sound name';
+
+
+    // T28 reference to the AudioSource we added earlier
+    private AudioSource SoundEffectsAudio;
+
+    // T28 use for initialization
+    // make sure there is only one SoundManager
     // Start is called before the first frame update
     void Start()
     {
-        
+        // if there is no Instance of the SoundManager, created the Instance
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+        // otherwise destroy one if there was another one created somehow
+        else if (Instance != null)
+        {
+            Destroy(gameObject);
+        }
+
+        // T28 get a hold of our audio source and assign it to soundEffectAudio instance
+        AudioSource theSource = GetComponent<AudioSource>();
+        SoundEffectsAudio = theSource;
     }
 
-    // Update is called once per frame
-    void Update()
+    // T28 the function any script can call, to play an audio clip
+    public void PlayOneShot(AudioClip clip)
     {
-        
+        SoundEffectsAudio.PlayOneShot(clip);
     }
 }
+
+
+
+
+/*
+* TEMPLATES TO PLAY SOUNDS IN SCRIPTS
+
+*  play a sound
+*  SoundManager.Instance.PlayOneShot(SoundManager.Instance.'sound name');
+
+*/

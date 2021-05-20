dba3ffb6f0e536718aa5c218e27f40f38a86c974
Rodman Underhill <robotrods@gmail.com>
2021-05-20 19:32:08 -0400

    update tutorial 30 published

--- Zelda/Assets/Scripts/Link2.cs
+++ Zelda/Assets/Scripts/Link2.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 // T26 Used so I can use Floor and round values
 using System;
 
+// T27 or T28 copied from Link.cs paste and altered here.
 public class Link2 : MonoBehaviour
 {
     // T26 think of evertything you want to tracked and/ or worked with, realated to Link
@@ -23,6 +24,20 @@ public class Link2 : MonoBehaviour
 
     // T26 access to change Links' animation
     Animator animator;
+
+    // T29 make references for the Sword and Weapon objects, so the sword will show up when in a chase
+    GameObject weaponGO;
+    GameObject swordGO;
+
+    // T29 track the direction we want to attack with enum
+    public enum AttkDir
+    {
+        LEFT, RIGHT, UP, DOWN
+    }
+
+    // T29 specifically have attack direction
+    AttkDir attkDir;
+
 
     // T26 change animations when Link is facing right, and we want to face left
     // using a bool, by default is false
@@ -45,6 +60,13 @@ public class Link2 : MonoBehaviour
 
         // T26 get component animator for access and to load it, which is already attached to Link
         animator = GetComponent<Animator>();
+
+        // T29 make references to load our weapon and sword, and find them by name, so we can work with them
+        weaponGO = GameObject.Find("Weapon");
+        swordGO = GameObject.Find("Sword");
+
+        // T29 get our sword component and render and disable it
+        //swordGO.GetComponent<Renderer>().enabled = false; // got rid of the error of Sword no being found by the script
     }
 
     // T26 fixedupdate to do all of our animations
@@ -63,19 +85,27 @@ public class Link2 : MonoBehaviour
         // T26 change animation, base on what key has been depressed
         if (Input.GetKey("s") || Input.GetKey("down"))
         {
-            // play walkDown animation
+            // T26 play walkDown animation
             animator.Play("WalkDown");
+
+            // T29 when link walks down, we will set our attack direction, and that will track him
+            attkDir = AttkDir.DOWN;
         }
         if (Input.GetKey("w") || Input.GetKey("up"))
         {
             // play walkDown animation
             animator.Play("WalkUp");
+
+            // T29 when link walks up, we will set our attack direction, and that will track him
+            attkDir = AttkDir.UP;
         }
         if (Input.GetKey("d") || Input.GetKey("right"))
         {
-            // play walkRight animation to move right
+            // T26 play walkRight animation to move right
             animator.Play("WalkRight");
 
+            /*
+             * commented out in T29
             // T26 find out weather the animation is facing left',
             // since the facingRight bool is set to False, set it to true since facing left
             // set the bool facinLeft to false, which is also its default setting
@@ -91,12 +121,22 @@ public class Link2 : MonoBehaviour
             {
                 facingRight = true;
             }
+            */
+
+            // T29 when link walks down, we will set our attack direction, and that will track him
+            attkDir = AttkDir.RIGHT;
         }
         if (Input.GetKey("a") || Input.GetKey("left"))
         {
-            // play walkRight animation to move left, by fliping it, below
-            animator.Play("WalkRight");
 
+            // T26 play walkRight animation to move left, by fliping it, below
+            animator.Play("WalkLeft");
+
+            // T29 when link walks left, we will set our attack direction, and that will track him
+            attkDir = AttkDir.LEFT;
+
+            /*
+             * T29 commented out
             // T26 find out weather the animation is facing right',
             // if it is facing right, we will set it to true, since the default is false
             // so that it will now face left
@@ -112,6 +152,8 @@ public class Link2 : MonoBehaviour
             {
                 facingLeft = true;
             }
+            */
+
         }
         // T26 change animation, base on what key has been released, for Idle
         if (Input.GetKeyUp("s"))
@@ -129,10 +171,72 @@ public class Link2 : MonoBehaviour
             // T26 anytime we set a position on the gameboard, or any game object, 
             // like Links position, we have to use a new vector
             transform.position = new Vector2(centerX, centerY);
-        }
 
+            // T29 get our sword component and render and disable it
+            swordGO.GetComponent<Renderer>().enabled = false;
+        }
+        // T29 when the user clicks the return key this will trigger the attack
+        if(Input.GetKey(KeyCode.Return))
+        {
+            // T29 call this function to attack
+            LinkAttack();
+        }
     }
 
+    // T29 Link attack what is in front of you, based on the information inside the attkDir enum
+    // LEFT, RIGHT, UP, DOWN
+    void LinkAttack()
+    {
+        // dependant upon Links attack direction LEFT, RIGHT, UP, DOWN
+        switch (attkDir)
+        {
+            case AttkDir.DOWN:
+                {
+                    // don't do anything, its our default action, pass 0,0 to coroutine
+                    StartCoroutine(AnimateSword(0f, 0f));
+                    break;
+                }
+            case AttkDir.LEFT:
+                {
+                    // move the sword to the left; -90 degrees, and down a bit
+                    StartCoroutine(AnimateSword(270f, -.5f));
+                    break;
+                }
+            case AttkDir.RIGHT:
+                {
+                    // move the sword to the right; 90 degrees
+                    StartCoroutine(AnimateSword(90f, 0f));
+                    break;
+                }
+            case AttkDir.UP:
+                {
+                    // move the sword so it faces up
+                    StartCoroutine(AnimateSword(180f, 0f));
+                    break;
+                }
+        }
+    }
+
+    // T29 coroutine to run with LinkAttack, to rotate the sword, 
+    // and if we want it to do a vertical movement
+    IEnumerator AnimateSword(float rotation, float vertMove)
+    {
+        // make the sword appear by refrencing it and enabling it
+        swordGO.GetComponent<Renderer>().enabled = true;
+
+        // we will rotate the sword by getting passed in the rotation value for z
+        // the Sword is inside the Weapon empty, which is inside the Link empty.
+        weaponGO.transform.rotation = Quaternion.Euler(0, 0, rotation);
+
+        // potntially move it vertically, when the sword goes from pointing downwards to upwards
+        weaponGO.transform.position = new Vector3(transform.position.x, transform.position.y + vertMove, transform.position.z);
+
+        // wait a second before hiding the sword again
+        yield return new WaitForSeconds(.2f);
+
+        // disable the sword so it dissapears
+        swordGO.GetComponent<Renderer>().enabled = false;
+    }
 
     // Start is called before the first frame update
     void Start()
---
+++ Zelda/Assets/Scripts/Villain.cs
@@ -0,0 +1,238 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Villain : MonoBehaviour
+{
+    // T29 define the villains speed, public so we can easily tweak it
+    public float speed = 2f;
+
+    // T29The villains will all start going right when loaded, and appear on screen
+    Vector2 direction = Vector2.right;
+
+    // T29 Keeps track of, if Villain is chasing Link, or not
+    // the villian will bounce back and forth between the VillainStart and VillainEnd Empty, points
+    // until a chase is initiated.
+    bool chasing = true;
+
+    // T29 Get reference to the Animator, so we can change the animations
+    private Animator animator;
+
+    // T29 we will create a Link GameObject later, but here is its reference.
+    // public so we can drag and drop the Link, into it later on in the scripts fields.
+    GameObject linkGO;
+
+    // T29 define Rigidbody 2d, to move Villain
+    Rigidbody2D rb;
+
+    // T30 array to hold all our travelNodes, by there position
+    int[,] villainNodes = new int[,]{
+        {2, -4}, { 11, -4 }, { 9,  0 }, { 2, 6 }, { 2,  16 }, { 8, 15 }, { 4, 11 }, { 5,  8 }, { 16, 9 }, { 20, 16 }, { 27, 10 }, { 25,  2 }, { 21, 5 }
+    };
+
+    // T30 access to our current Villain node, in our index
+    public int currentNode = 1;
+
+    // track where are target node is, where Villain is going to go to
+    public int targetNode = 2;
+
+    // T30 array to keep track of the Node nearest to Link, by the Nodes index value
+    GameObject[] villainNodeGOs;
+
+    // T30 store the Nearest Nodes location that is nearest to Link
+    GameObject linksClosestNodeGO;
+
+    // T29 initialize things, so they are setup and ready when called upon to perform
+    void Awake()
+    {
+        // get our reference to the Link Component, for our linkGO,
+        // use find to get the only Link character in the game
+        linkGO = GameObject.Find("Link");
+
+        // setup the rigidbody 2d
+        rb = GetComponent<Rigidbody2D>();
+
+        // setup the animator
+        animator = GetComponent<Animator>();
+
+        // T30 initialize villainNodeGOs array, to get all the Nodes
+        villainNodeGOs = GameObject.FindGameObjectsWithTag("Villain");
+
+    }
+
+    // T29 fixed update will call our rigidbody and change its velocity, on a regular basis
+    void FixedUpdate()
+    {
+        // change rigidbody2d velocity
+        //rb.velocity = direction * speed; //T30 commented out and moved to the Update()
+        //GetComponent<Rigidbody2D>().velocity = direction * speed;
+
+        // T30 make the nodes work for now, using a key, and automate them later
+        // watch how Villain moves between the nodes, by hitting the f key, and going forward,
+        // to debug, if it gets caught on objects
+        if (Input.GetKeyUp("f"))
+        {
+            // go to a node that is less than 12 index spaces away out of 13
+            if (targetNode < 12)
+            {
+                // as long as we have 1 node left before the array runs out, 
+                // we will will increment our targetNode, to keep count
+                targetNode = targetNode + 1;
+
+                // use our function to move forward to that next node
+                GoForwardToNode();
+
+                // T30 output Links position, to our array, using our function LinksClosestNodeGO() to get the closest GameObject Node
+                linksClosestNodeGO = LinksClosestNode();
+
+                // T30 we also will want the GameObjects node index,
+                Debug.Log("Links closest Nodes index # " + GetIndexForLinksClosestNode(linksClosestNodeGO));
+            }
+
+        }
+        // T30 go backwards through the nodes, to debug
+        if (Input.GetKeyUp("b"))
+        {
+            // go to a node that is less than 12 index spaces away out of 13
+            if (targetNode > 0)
+            {
+                // as long as we have 1 node left before the array gets to index[0], 
+                // we will will decrement our targetNode, to keep count
+                targetNode = targetNode - 1;
+
+                // use our function to move forward to that next node
+                GoForwardToNode();
+            }
+        }
+    }
+    // T30 setup the way to move the Villain to the next node
+    void GoForwardToNode()
+    {
+        // if we are Not already at the Target Node, we want to go there
+        if (!AtTargetNode())
+        {
+            // get the x and y positions, that we want to move to, for our TargetNode, from our villainNodes array
+            // by subtracting Villains current position from the targetNodes position
+            Vector2 targetPosition = new Vector2(villainNodes[targetNode, 0] - transform.position.x, villainNodes[targetNode, 1] - transform.position.y);
+
+            Debug.Log("Target X : " + villainNodes[targetNode, 0] +
+            "Y : " + villainNodes[targetNode, 1]);
+
+            Debug.Log("Target Node : " + targetNode);
+
+            // set the direction to our target
+            direction = targetPosition;
+        }
+    }
+
+    // T30 how to check if Villain is at the target node
+    bool AtTargetNode()
+    {
+        // if Villains x posiion is equal to the x position of VillainNodes targetNode 0 (0 means the x position) 
+        // and Villains y position equals the y position of the VillainNodes targetNode 1 (1 means the x position)
+        // its at the targetNode
+        if (transform.position.x == villainNodes[targetNode, 0] && transform.position.y == villainNodes[targetNode, 1])
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    // T29 handle when our Villain hits one of those triggers
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        // we hit a trigger now what
+        // find out if we where chasing  or not,
+        // if we where not chasing, and collided with the VillainStart or VillainEnd
+        // we will change the x direction our Villain is facing, by flipping it, which reacts super quick
+        if (!chasing)
+        {
+            transform.localScale = new Vector2(-1 * transform.localScale.x,
+                transform.localScale.y);
+
+            // also change the direction that we are moving in
+            direction = new Vector2(-1 * direction.x, direction.y);
+        }
+        // TODO else is for when our villain is chasing Link
+        else
+        {
+
+        }
+
+        // T30 check to see if our Villain collided with a VillainNode
+        if (col.tag == "VillainNode")
+        {
+            // and if it is the TargetNode, then stop moving
+            if (col.transform.position.x == villainNodes[targetNode, 0] && col.transform.position.y == villainNodes[targetNode, 1])
+            {
+                direction = Vector3.zero;
+            }
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // T30 change rigidbody2d velocity, moved here from FixedUpdate()
+        rb.velocity = direction;
+    }
+
+    // T30 get the node that is nearest to Link, by returning that Node
+    GameObject LinksClosestNode()
+    {
+        // The closest node will start off with a value of null
+        GameObject closestNodeGo = null;
+
+        // cycle through all of our villain Node game objects
+        foreach (GameObject nodeGO in villainNodeGOs)
+        {
+            // if the node has no value
+            if (closestNodeGo == null)
+            {
+                // then we will make the first one equal to null, 
+                // then continue cycling through the list, from there, till we find one that isn't null
+                closestNodeGo = nodeGO;
+            }
+            // check the distance between Links and the Nodes, the current node we are cycling through
+            // or the last one that wasn't null, to see which node, of the two is closest to link
+            if (Vector3.Distance(linkGO.transform.position, nodeGO.transform.position) <= Vector3.Distance(linkGO.transform.position, closestNodeGo.transform.position))
+            {
+                // Assign the closest gameobject for comparison
+                closestNodeGo = nodeGO;
+            }
+        }
+        // return our closest node
+        return closestNodeGo;
+    }
+
+    // T30 get the index for the closest node that we found, above in LinksClosestNode()
+    // this will return an int
+    int GetIndexForLinksClosestNode(GameObject linksNodeGO)
+    {
+        int nodeIndex = 0;
+        for (int i = 0; i <= villainNodes.GetUpperBound(0); i++)
+        {
+            Debug.Log("X : " + villainNodes[i, 0]);
+            Debug.Log("Y : " + villainNodes[i, 1]);
+
+            // check inside the index for value of x at the index of i, 0 is the x position
+            // check inside the index for value of y at the index of i, 1 is the y position
+            if (linksNodeGO.transform.position.x == villainNodes[i, 0] &&
+            linksNodeGO.transform.position.y == villainNodes[i, 1])
+            {
+                // if both x and y are true assign the index number to nodeIndex
+                nodeIndex = i;
+            }
+        }
+        return nodeIndex;
+    }
+}
